d14f4d72e49cfc3f7d7477e59848955ff769a270
Fast <faustogarcia1357@gmail.com>
2024-04-05 19:46:33 -0300

    intento 3 sacando el header

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
 //css
 import "./App.css"
 
-//React Router Dom
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-
 //componentes
 import { Footer } from "./components/Footer/Footer.jsx"
-import { Header } from "./components/Header/Header.jsx"
 import { Principal } from "./Principal"
-// import { FAQ } from "./pages/FAQ/FAQ"
+
 function App() {
   return (
     <>
-      <Header />
       <Principal />
       <Footer />
     </>
-
-    // <Router>
-    //   <Header />
-    //   <Routes>
-    //     <Route path='/' element={<Principal />}></Route>
-    //     <Route path='/FAQ' element={<FAQ />}></Route>
-    //   </Routes>
-    //   <Footer />
-    // </Router>
   )
 }
 
