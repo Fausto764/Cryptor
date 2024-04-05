61240ad70ef6f4777d6cdb4481ff65f9bfdaf097
Fast <faustogarcia1357@gmail.com>
2024-04-05 20:40:56 -0300

    prueba nombre

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import { Informacion } from "./pages/Informacion/Informacion"
 import { Inicio } from "./pages/Inicio/Inicio"
 import { Servicios } from "./pages/Servicios/Servicios"
 //componentes
-
+import { Header } from "./components/Headerx/Header.jsx"
 import { Footer } from "./components/Footer/Footer.jsx"
 
 function App() {
   return (
     <>
+      <Header />
       <main>
         <Inicio />
         <Informacion />
